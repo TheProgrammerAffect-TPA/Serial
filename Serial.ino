0ed463775cfb79ba17e026692a0bc9b588211b76
ZU6X <67498572+ZU6X@users.noreply.github.com>
2020-07-04 16:53:42 +0200

    Update Serial.ino

--- Serial.ino
+++ Serial.ino
@@ -1,49 +1,69 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(2, 3); // RX | TX
+// Connect the HC-05 TX to Arduino pin 2 RX. 
+// Connect the HC-05 RX to Arduino pin 3 TX through a voltage divider.
+// 
+
+char A = ' ';
+
 int incomingByte = 0;
 int led1 = 13;
-int led2 = 2;
+int led3 = 2;
 
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("Arduino is ready");
+    Serial.println("Remember to select Both NL & CR in the serial monitor");
+ 
+    // HC-05 default serial speed for Command mode is 9600
+    BTserial.begin(115200);  
   pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
 
 }
 
 void loop() {
   if (Serial.available())  {
+    if (BTserial.available()) {
+       BTserial.read();
+        
+    }
     //Read incoming Byte:
     incomingByte = Serial.read();
 
+    
+
     switch (char(incomingByte)) {
+      default:
       case 'A':
 
       Serial.println(" LED 1  ON");
       ledOn(led1);
-      break;
+      
+    //  break;
 
    case 'a':
    Serial.println(" LED 1  OFF");
    ledOff(led1);
    break;
 
-   case 'B':
+ case 'B':
+   Serial.println(" LED 3 ON"); 
+   ledOn(led3);
+   
 
-      Serial.println(" LED 2 ON");
-      ledOn(led2);
-      break;
-
-   case 'b':
-   Serial.println(" LED 2 OFF");
-   ledOff(led1);
    break;
- default:
-   Serial.println("Oops");  
+  
+ case 'b':
+
+   Serial.println(" LED 3 OFF"); 
+   ledOff(led3);
+
+   break;
     }
 
-    // Say what you got:
-   // Serial.print("I recieved: ");
-  //  Serial.println(char(incomingByte));
+    
   }
 }
 
